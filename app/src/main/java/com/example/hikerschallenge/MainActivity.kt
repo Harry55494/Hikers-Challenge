664b69708b91a0e8411793e2595c0093cd3ab672
Harrison <harrison@phillingham.com>
2023-11-14 11:35:15 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/hikerschallenge/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.hikerschallenge
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var tag = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+
+
+
+
+
+}
